db1b8b4b2286391da27ae4558878b7f299608d70
Hoda <hoda.shariati@outlook.com>
2025-02-06 10:49:59 +0100

    Add terraform configuration for VM creation

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-
-
 terraform {
   required_providers {
     google = {
@@ -10,6 +8,41 @@ terraform {
 }
 
 provider "google" {
-  project     = "fintech-dashboard-terraform"
-  region      = "europe-west1"
+  project = "fintech-dashboard-terraform"
+  region  = "europe-west1-c"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "europe-west1-c"  # Changed to Belgium
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
   }
+
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+
+  service_account {
+    email  = "terraform-runner@fintech-dashboard-terraform.iam.gserviceaccount.com"  # Use existing SA
+    scopes = ["cloud-platform"]
+  }
+}
+
