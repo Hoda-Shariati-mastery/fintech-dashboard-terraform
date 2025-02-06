69c713dd52822658807cf13c27098b5e4ebef9b5
Hoda <hoda.shariati@outlook.com>
2025-02-06 12:42:38 +0100

    Added Cloud Storage bucket configuration to Terraform

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "google" {
   project = "fintech-dashboard-terraform"
-  region  = "europe-west1-c"
+  region  = "europe-west1"
 }
 
 resource "google_compute_instance" "default" {
@@ -43,6 +43,25 @@ resource "google_compute_instance" "default" {
   service_account {
     email  = "terraform-runner@fintech-dashboard-terraform.iam.gserviceaccount.com"  # Use existing SA
     scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "fintech-dashboard-terraform"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
   }
 }
 
